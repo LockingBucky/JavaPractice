90336d38763c41a2a7d77b6e6cac6ed13fc5f87b
Bucky <789werop@gamil.com>
2022-06-11 17:47:19 +0900

    While for문 연습

--- JavaPractice/src/ch04/Test4_3.java
+++ JavaPractice/src/ch04/Test4_3.java
@@ -3,8 +3,16 @@ package ch04;
 public class Test4_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int sum =0;
+		int total = 0;
+		
+		int i = 1;
+		while (i <= 10) {
+			sum += i;
+			total += sum;
+			i++;
+		}
+		System.out.println(total);
 	}
 
 }
--- JavaPractice/src/ch04/Test4_4.java
+++ JavaPractice/src/ch04/Test4_4.java
@@ -3,7 +3,16 @@ package ch04;
 public class Test4_4 {
 
 	public static void main(String[] args) {
-
+		int sum = 0;
+		int j = 1;
+		int num = 0;
+		
+		for (int i = 1; sum < 100; i++, j = -j) {
+			num = i * j;
+			sum += num;
+		}
+		System.out.println(num);
+		System.out.println(sum);
 	}
 
 }
